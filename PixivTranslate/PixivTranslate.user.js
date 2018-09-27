6fc0adf1fabf04bb2b3d0f25df2a63c3328a4dcc
LEORChn <a522237296@gmail.com>
2018-09-27 11:40:08 +0800

    pixiv翻译 v1.0.2
    新增词库：动图、多层上色、狼、毛怪、犬塚戍孝、诺曼德（东放）、腹肌
    新增作品详情页对标题和说明使用的谷歌翻译

--- PixivTranslate/PixivTranslate.user.js
+++ PixivTranslate/PixivTranslate.user.js
@@ -1,18 +1,22 @@
 // ==UserScript==
 // @name         Pixiv 辅助翻译
 // @namespace    https://greasyfork.org/users/159546
-// @version      1.0.1
-// @description  目前只有标签翻译这样
+// @version      1.0.2
+// @description  目前只有标签翻译这样。以及作品详情页对标题和说明使用的谷歌翻译
 // @author       LEORChn
 // @include      *://www.pixiv.net/*
 // @run-at       document-start
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 var tag_trans=[
     '漫画','漫画',
+    'うごイラ','动图',
+    '厚塗り','多层上色',
     'UTAU獣人','兽人虚拟歌手',
     '狼音アロ','狼音阿罗',
     'ケモノ','野兽',
+    'wolf','狼',
+    'kemono','毛怪',
     'furry','兽人',
     '獣人','兽人', // 这个是日文的
     '獸人','兽人', // 这个是繁体的
@@ -24,12 +28,15 @@ var tag_trans=[
     '放サモ','东放',
     'housamo','东放',
     'モリタカ','犬塚戍孝',
+    '犬塚モリタカ','犬塚戍孝',
     'ホロケウカムイ','狼神神威', //ID为 20880639 的作者似乎打错了这个标签所以替换不到
     'テムジン','铁木真',
     'ガルム','加姆',
+    'ノーマッド(放サモ)','诺曼德（东放）', // 东京放课后里的虎兽人所用的化名
     '主5','5号主人公（东放）',
     'ふんどし','兜裆布',
     '褌','兜裆布',
+    '腹筋','腹肌',
     'ㄋㄟㄋㄟ','胸部',
     '金的','捣蛋',
     '猿轡','封口',
@@ -60,7 +67,9 @@ function main_do(){
     tagTranslate_bookmark(); // 个人空间的收藏列表页面的标签
     tagTranslate_addBookmark(); // 添加收藏时的可选择标签
     tagTranslate_member_tag_all();
+    detailTranslate_illust();
 }
+// ----- （接近全站的）标签分界线
 function tagTranslate_illust_single(){
     if(location.pathname != '/member_illust.php')return;
     var tags=ft('footer');
@@ -137,6 +146,64 @@ function tagDict(origin){
         if(origin==tag_trans[i])
             return tag_trans[i+1];
 }
+// ----- 作品页标题和说明分界线
+function detailTranslate_illust(){
+    if(location.pathname != '/member_illust.php')return;
+    if(!location.href.includes('mode=medium'))return;
+    var tags=ft('footer');
+    if(tags.length==0 || fv('icon_google_translate'))return;
+    tags=tags[0].parentNode;
+    var img=ct('img');
+    img.id='icon_google_translate';
+    img.src='https://translate.google.cn/favicon.ico';
+    img.style.cssText='width:24px;float:right';
+    tags.childNodes[0].appendChild(img);
+    img.onclick=function(){}
+    trans_launcher=img;
+    if(!stat_title) trans_title();
+    if(!stat_desc) trans_desc();
+}
+var trans_launcher;
+var stat_title,
+    stat_desc;
+function trans_title(){
+    stat_title=true;
+    var p=trans_launcher.parentNode;
+    googleTranslate(p.innerText,
+      function(r){
+        if(!r.responseText || r.responseText.length<20){
+            stat_title=false;
+            trans_title();
+            return;
+        }
+        trans_launcher.parentNode.appendChild(
+            trans_create_block(r.responseText)
+        );
+      }
+    );
+}
+function trans_desc(){
+    stat_desc=true;
+    var p=trans_launcher.parentNode.parentNode.childNodes[1];
+    googleTranslate(p.innerText,
+      function(r){
+        if(!r.responseText || r.responseText.length<20){
+            stat_desc=false;
+            trans_desc();
+            return;
+        }
+        trans_launcher.parentNode.parentNode.childNodes[1].appendChild(
+            trans_create_block(r.responseText)
+        );
+      }
+    );
+}
+function trans_create_block(oriText){
+    var n=ct('p');
+    n.innerText=googleTranslate_get(oriText);
+    n.style.backgroundColor='#d0ffd0';
+    return n;
+}
 //----- my ezjs lib
 function fv(id){return document.getElementById(id);}
 function ft(tag){return document.getElementsByTagName(tag);}
@@ -145,3 +212,19 @@ function ct(tag){return document.createElement(tag);}
 function msgbox(msg){alert(msg);}
 function inputbox(title,defalt){return prompt(title,defalt);}
 function pl(s){console.log(s);}
+function googleTranslate_get(origin){
+    return new RegExp('<div\\s.{0,15}class=\\"t0\\".{0,15}>(.*)<\\/div><f').exec(origin)[1];
+}
+function googleTranslate(word,dofun,dofail){
+    http('get','https://translate.google.cn/m?hl=zh-CN&sl=auto&tl=zh-CN&ie=UTF-8&prev=_m&q='+encodeURI(word),'',dofun,dofail);
+}
+function http(_method,_url,formdata,dofun,dofail){
+    GM_xmlhttpRequest({
+        method: _method.toUpperCase(),
+        url: _url,
+        data: formdata,
+        headers:{},
+        onload: dofun,
+        onerror: dofail
+    });
+}
