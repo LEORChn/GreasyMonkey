c9f4cc0fbd5e3b529f83920db3dac7d5966064c1
LEORChn <a522237296@gmail.com>
2020-10-30 01:09:23 +0800

    v1.0.3 添加微软待办
    1.0.2 之前没有推送过吗？添加了超能搜布局的修复。

--- ScrapToolbox.user.js
+++ ScrapToolbox.user.js
@@ -2,25 +2,29 @@
 // @name              ScrapToolbox
 // @description       what a scrap!
 // @name:zh-CN        乱七八糟工具箱
-// @description:zh-CN 这里面有一堆用来兼容旧浏览器的垃圾！包括：大圣盘直链显示、微博新版界面修复、百度知道展开折叠
-// @version           1.0.1
+// @description:zh-CN 这里面有一堆用来兼容旧浏览器的垃圾！包括：微软待办布局修复、超能搜布局修复、大圣盘直链显示、微博新版界面修复、百度知道展开折叠
+// @version           1.0.3
 // @namespace         https://greasyfork.org/users/159546
 // @author            LEORChn
 // @include           *://zhidao.baidu.com/*
 // @include           *://weibo.com/*
 // @include           *://www.dashengpan.com/detail/*
+// @include           *://www.chaonengsou.com/*
+// @include           *://to-do.live.com/tasks/*
 // @require           https://greasyfork.org/scripts/401996-baselib/code/baseLib.js?version=835697
 // @run-at            document-body
 // @grant             GM_xmlHttpRequest
 // @connect           translate.google.cn
 // ==/UserScript==
 var DEBUG = 0
-? 'http://127.0.0.1:81': 'https://leorchn.github.io';
+? 'https://127.0.0.1:81': 'https://leorchn.github.io';
 var IntervalTime = 2000;
 function onIntervalFunction(){
     zhidao.baidu.com();
     weibo.com();
+    www.chaonengsou.com();
     www.dashengpan.com();
+    todo.live.com();
 }
 var zhidao = { baidu: { com: function(){
     if(location.hostname != 'zhidao.baidu.com') return;
@@ -49,6 +53,19 @@ www = { dashengpan: { com: function(){
     a.rel = 'noreferrer';
     info_place.appendChild(div);
     div.appendChild(a);
+}},
+chaonengsou: { com: function(){
+    if(location.hostname != 'www.chaonengsou.com') return;
+    var eid = 'leorchn_chaonengsou_stylesheet';
+    if($('#' + eid)) return;
+    appendCSS(DEBUG + '/app/external/www.chaonengsou.com.css').id = eid;
+}}
+},
+todo = { live:{ com: function(){
+    if(location.hostname != 'to-do.live.com') return;
+    var eid = 'leorchn_microsoft_to_do_stylesheet';
+    if($('#' + eid)) return;
+    appendCSS(DEBUG + '/app/external/to-do.live.com.css').id = eid;
 }}};
 
 // ===== =====
