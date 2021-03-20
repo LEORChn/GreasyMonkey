fa89cb04a9a9a2b2bb41b2e4dd2fd41bff23012e
LEORChn <a522237296@gmail.com>
2021-03-20 12:48:05 +0800

    1.0.10 - 更改必应实现。。

--- ScrapToolbox.user.js
+++ ScrapToolbox.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name              ScrapToolbox
-// @description       what a scrap!
+// @description       switch to Chinese for details. this might not for English users.
 // @name:zh-CN        乱七八糟工具箱
-// @description:zh-CN 这里面有一堆用来兼容旧浏览器的垃圾！包括：必应修复BUG滚屏、V2EX界面修复、推特界面修复、Github新版布局修复、微软待办布局修复、超能搜布局修复、大圣盘直链显示、微博新版界面修复、百度知道展开折叠
-// @version           1.0.9
+// @description:zh-CN 这里面有一堆用来兼容旧浏览器的垃圾！包括：必应图片详情页修复、必应滚屏BUG修复、V2EX界面修复、推特界面修复、Github新版布局修复、微软待办布局修复、超能搜布局修复、大圣盘直链显示、微博新版界面修复、百度知道展开折叠
+// @version           1.0.10
 // @namespace         https://greasyfork.org/users/159546
 // @author            LEORChn
 // @include           *://zhidao.baidu.com/*
@@ -20,8 +20,8 @@
 // @include           *://www.v2ex.com/*
 // @include           *://cn.bing.com/*
 // @require           https://greasyfork.org/scripts/401996-baselib/code/baseLib.js?version=835697
-// @require           https://greasyfork.org/scripts/401997-http/code/http.js?version=797848
-// @require           https://127.0.0.1:81/app/external/github.com.js?12
+// @require-           https://greasyfork.org/scripts/401997-http/code/http.js?version=797848
+// @require-           https://127.0.0.1:81/app/external/github.com.js?16
 // @run-at            document-body
 // @grant             GM_xmlhttpRequest
 // @connect           127.0.0.1
@@ -55,24 +55,51 @@ var zhidao = { baidu: { com: function(){
 }}},
 cn = { bing: { com: function(){
     if(location.hostname != 'cn.bing.com') return;
-    var runned;
-    if(runned = $('head[leorchn_bing_onfocus_scroll_fix]')) return;
-    document.head.setAttribute('leorchn_bing_onfocus_scroll_fix', '');
-    pl('LEORChn Bing onfocus scroll fix is protecting.'); // 必应的搜索结果页面有时会在从其他页面返回搜索结果页面时执行 scrollTo(0, 0) 暂且不知原因，因此针对这个bug做了个修复
-    var memScrollY = 0;
-    window.addEventListener('blur', function(){ // 在离开页面时记录纵向滚动位置
-        memScrollY = window.scrollY;
-    });
-    window.addEventListener('focus', function(){ // 在重返页面时的前 0.5 秒内执行 10 次将将页面纵向滚动到离开前位置
-        var count = 0;
-        setTimeout(onfocusRun, 50);
-        function onfocusRun(){
-            window.scrollTo(window.scrollX, memScrollY);
-            count++;
-            if(count > 9) return;
+    (function(){
+        if($('head[leorchn_bing_onfocus_scroll_fix]')) return;
+        document.head.setAttribute('leorchn_bing_onfocus_scroll_fix', '');
+        pl('LEORChn Bing onfocus scroll fix is protecting.'); // 必应的搜索结果页面有时会在从其他页面返回搜索结果页面时执行 scrollTo(0, 0) 暂且不知原因，因此针对这个bug做了个修复
+        var memScrollY = 0;
+        window.addEventListener('blur', function(){ // 在离开页面时记录纵向滚动位置
+            memScrollY = window.scrollY;
+        });
+        window.addEventListener('focus', function(){ // 在重返页面时的前 0.5 秒内执行 10 次将将页面纵向滚动到离开前位置
+            var count = 0;
             setTimeout(onfocusRun, 50);
-        };
-    });
+            function onfocusRun(){
+                window.scrollTo(window.scrollX, memScrollY);
+                count++;
+                if(count > 9) return;
+                setTimeout(onfocusRun, 50);
+            };
+        });
+    })();
+    (function(){
+        if(unsafeWindow == unsafeWindow.top) return;
+        var host = $('#detailMeta:empty');
+        if(!host) return;
+        var picid = /\/id\/(.*?)(\?|$)/.exec(document.querySelector('img[aria-label]').src)[1];
+        console.log(picid);
+        for(var i = 0, a = unsafeWindow.top.document.querySelectorAll('a[m]'); i<a.length; i++){
+            var thisinf = a[i].getAttribute('m');
+            if(!thisinf.includes(picid)) continue;
+            var inf = JSON.parse(thisinf);
+            var caption = ct('a.ptitle novid', inf.t);
+            caption.href = inf.purl;
+            caption.setAttribute('target', '_blank');
+            var imagemeta = ct('div#imagemeta'),
+                imgurl = ct('a', inf.murl);
+            imgurl.href = inf.murl;
+            imgurl.setAttribute('target', '_blank');
+            imagemeta.appendChildren(
+                ct('a', caption.hostname),
+                ct('span.meta_sp', '|'),
+                imgurl
+            );
+            host.appendChildren(caption, imagemeta);
+            return;
+        }
+    })();
 }}},
 weibo = { com: function (){
     if(location.hostname != 'weibo.com') return;
@@ -120,6 +147,7 @@ github = { com: function(){
     injectInlineCSS('leorchn_github_fragment',           'github.com.fragment.css' + DBG);
     injectInlineCSS('leorchn_github_profile',            'github.com.profile.css' + DBG);
     injectInlineCSS('leorchn_github_repository',         'github.com.repository.css' + DBG);
+    injectInlineCSS('leorchn_github_repository_code',    'github.com.repository.code.css' + DBG);
     injectInlineCSS('leorchn_github_repository_commits', 'github.com.repository.commits.css' + DBG);
 }},
 greasyfork = { org: function(){
@@ -169,6 +197,62 @@ function appendJS(url){
     s.src = url;
     htmlhead.appendChild(s);
     return s;
+}
+function http(){
+    var args = Array.prototype.slice.call(arguments);
+    if(args.length == 1 && args[0] instanceof Array) args = args[0];
+    var pointer = 0,
+		method, url, headers, formdata, dofun, dofail, onprogress;
+	args.forEach(function(e){
+		switch(pointer){
+			case 0:
+				e = e.split(' ', 2); // 允许在第一个参数中用空格将 http-method 与 url 隔开，而省去 引号+逗号+引号 的麻烦
+				method = e[0].toUpperCase();
+				if(e.length > 1){
+					pointer++; // 偏移到下一个
+					e = e[1];
+				}else break;
+			case 1: url = e; break;
+            case 2:
+            case 3:
+				if(e instanceof Function){ // 允许不添加 http-body 而直接撰写行为。
+					pointer = 4; // 偏移到function
+				}else{
+                    if(pointer == 2)
+                        headers = e;
+                    else if(pointer == 3)
+                        formdata = e;
+					break;
+				}
+			case 4: dofun = e; break;
+			case 5: dofail = e; break;
+			case 6: onprogress = e;
+		}
+		pointer++;
+	});
+    var x = new XMLHttpRequest()
+	if(location.protocol.includes('https')) url = url.replace('^http:', 'https:');
+	x.open(method, url, true);
+	x.timeout = 60000;
+	x.responseType = "text"; // IE要求先open才能设置timeout和responseType
+	x.onload = dofun;
+	x.ontimeout = x.onerror = dofail? dofail: null;
+	x.onprogress = onprogress;
+	if(formdata) x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    else formdata = '';
+    var headersobj = getHeaders(headers);
+    for(var hobj in headersobj) x.setRequestHeader(hobj, headersobj[hobj]);
+	x.send(formdata);
+}
+function getHeaders(t){
+    var obj = {};
+    if(!t) return obj;
+    t.split('\n').forEach(function(e){
+        var res, reg = /\s*(\S+)\s*:\s*(.*)/;
+        if((res = reg.exec(e)) == null) return;
+        obj[res[1]] = res[2];
+    });
+    return obj;
 }
 function http2(_method, _url, formdata, dofun, dofail){
     console.log(_method.toUpperCase()+' '+_url+(formdata && formdata.length > 0 ? '\nform: '+formdata:'') +'\n\n.');
